74305cb094cda3ebd2cece8ee10f3fc040592a12
Per Liedman <per.liedman@dotnetmentor.se>
2018-09-15 21:36:51 +0200

    Upgrade to Svelte 2

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,17 @@ import json from 'rollup-plugin-json'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
-import sizes from 'rollup-plugin-sizes'
 import url from "rollup-plugin-url"
 
 export default {
   input: 'src/main.js',
-  external: ['mapbox-gl-js'],
+  external: ['mapbox-gl'],
   output: {
     file: 'dist/mapbox-gl-routing-machine.js',
     format: 'umd',
     name: 'mbglrm',
     globals: {
-      'mapbox-gl-js': 'mapboxgl'
+      'mapbox-gl': 'mapboxgl'
     }
   },
   plugins: [
@@ -25,7 +24,6 @@ export default {
       },
     }),
     resolve({browser: true}),
-    commonjs(),
-    sizes()
+    commonjs()
   ]
 }
